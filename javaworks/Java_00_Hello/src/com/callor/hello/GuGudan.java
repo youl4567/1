3859f0e7e2b115b6a45960cf1ab6b33061aadf0c
youl4567 <youl4567@gmail.com>
2022-03-23 13:54:49 +0900

    코멘트

---
+++ javaworks/Java_00_Hello/src/com/callor/hello/GuGudan.java
@@ -0,0 +1,22 @@
+package com.callor.hello;
+
+public class GuGudan {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("구구단 3단");
+		System.out.println("====================");
+		System.out.println("3 X 2 = " + 3*2);
+		System.out.println("3 X 3 = " + 3*3);
+		System.out.println("3 X 4 = " + 3*4);
+		System.out.println("3 X 5 = " + 3*5);
+		System.out.println("3 X 6 = " + 3*6);
+		System.out.println("3 X 7 = " + 3*7);
+		System.out.println("3 X 8 = " + 3*8);
+		System.out.println("3 X 9 = " + 3*9);
+		System.out.println("===================");
+		
+	}
+	
+
+}
