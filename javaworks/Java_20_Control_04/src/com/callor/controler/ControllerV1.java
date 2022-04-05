0f1d536d78ec63cd4eb155f0a899eca2653adacc
windowyoul <windowyoul@gmail.com>
2022-04-05 12:48:30 +0900

    4/5

---
+++ javaworks/Java_20_Control_04/src/com/callor/controler/ControllerV1.java
@@ -0,0 +1,14 @@
+package com.callor.controler;
+
+import com.callor.service.ServiceV1;
+
+public class ControllerV1 {
+
+	public static void main(String[] args) {
+		
+		ServiceV1 V1 = new ServiceV1();
+		
+		V1.gugudan();
+		
+		}
+}
---
+++ javaworks/Java_20_Control_04/src/com/callor/service/ServiceV1.java
@@ -0,0 +1,23 @@
+package com.callor.service;
+
+public class ServiceV1 {
+	/*
+	 * void type method를 gugudan 이름으로 선언하기
+	 * void type method 는 return 명령이 없어도 된다.
+	 */
+	public void gugudan() {
+		
+		int intNum1 = 1;
+		System.out.printf("%d X %d = %d\n", 3, intNum1, 3 * intNum1++);
+		System.out.printf("%d X %d = %d\n", 3, intNum1, 3 * intNum1++);
+		System.out.printf("%d X %d = %d\n", 3, intNum1, 3 * intNum1++);
+		System.out.printf("%d X %d = %d\n", 3, intNum1, 3 * intNum1++);
+		System.out.printf("%d X %d = %d\n", 3, intNum1, 3 * intNum1++);
+		System.out.printf("%d X %d = %d\n", 3, intNum1, 3 * intNum1++);
+		System.out.printf("%d X %d = %d\n", 3, intNum1, 3 * intNum1++);
+		System.out.printf("%d X %d = %d\n", 3, intNum1, 3 * intNum1++);
+		System.out.printf("%d X %d = %d\n", 3, intNum1, 3 * intNum1++);
+		
+	}
+
+}
