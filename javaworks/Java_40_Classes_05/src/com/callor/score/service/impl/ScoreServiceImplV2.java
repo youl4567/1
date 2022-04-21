23abc4f8e5fd21ab9f2865aa5cf50a611435b8f3
windowyoul <windowyoul@gmail.com>
2022-04-21 12:26:58 +0900

    04/21

--- javaworks/Java_40_Classes_05/src/com/callor/score/service/impl/ScoreServiceImplV2.java
+++ javaworks/Java_40_Classes_05/src/com/callor/score/service/impl/ScoreServiceImplV2.java
@@ -4,4 +4,28 @@ import com.callor.score.service.ScoreService;
 
 public class ScoreServiceImplV2 implements ScoreService {
 
+	@Override
+	public void makeScore() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void printScore() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int returnSum() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float retAvg() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
